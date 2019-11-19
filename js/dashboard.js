ad4b8e86670cc5eaa9a1578dc32cdebbddca7be5
Todd Murphy <tam1998@comcast.net>
2019-11-18 19:08:41 -0500

    fixed radio buttons

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,8 @@
 function showResumeUpload(){
-    
+    document.getElementById('dashResumeContainer').style.display = "block"
+    document.getElementById('dashProjectsContainer').style.display = "none"
+}
+function showProjectUpload(){
+    document.getElementById('dashResumeContainer').style.display = "none"
+    document.getElementById('dashProjectsContainer').style.display = "block"
 }
