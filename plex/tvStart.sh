7d9a45394eb5d2d023fb58224066e6d0714292d5
Todd Murphy <tam1998@comcast.net>
2020-03-15 22:52:13 -0400

    updated scripts

--- plex/tvStart.sh
+++ plex/tvStart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Generating playlist for Todderang"
+echo "Generating playlist for Channel"
 
-python3 /mnt/plexNAS/Files/tv/generatePlaylist.py no
+python3 /path/to/tv/generatePlaylist.py no
 
 sleep 5
 
-/usr/bin/cvlc /mnt/plexNAS/Files/tv/playlist.m3u --sout '#transcode{vcodec=h264, acodec=mp3, vb=800, ab=128} :standard{access=http, mux=ts, dst=192.168.1.43:1234}' --no-sout-all --sout-mux-caching=10000 &
+/usr/bin/cvlc /path/to/tv/playlist.m3u --sout '#transcode{vcodec=h264, acodec=mp3, vb=800, ab=128} :standard{access=http, mux=ts, dst=<ip:port>}' --no-sout-all --sout-mux-caching=10000 &
 
 sleep 5
 
@@ -13,4 +13,4 @@ echo -ne '\n'
 
 sleep 5
 
-python3 /mnt/plexNAS/Files/tv/generatePlaylist.py yes
+python3 /path/to/tv/generatePlaylist.py yes
